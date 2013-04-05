3c5b88869c0aa66d4bf8979a08ede7c3ca003740
Mathieu Lecarme <mlecarme@bearstech.com>
2013-04-06 00:11:50 +0200

    Private callback got its _. Some cleaning.

--- src/web.py
+++ src/web.py
@@ -1,4 +1,5 @@
 from tornado.web import RequestHandler, asynchronous, Application
+from tornado.httpserver import HTTPServer
 from tornado.httpclient import HTTPClient
 from tornado.ioloop import IOLoop, PeriodicCallback
 # Inspiration from
@@ -37,7 +38,7 @@ class GraphiteClient(object):
     def _fetch(self):
         for url in self.targets:
             response = self.client.fetch(url)
-            self.events.emit('*', len(response.body))
+            self.events.emit('*', response.body)
 
     def watch_for(self, url):
         self.targets.add(url)
@@ -48,36 +49,32 @@ class EventSourceHandler(RequestHandler):
     def initialize(self, events, clients):
         self.events = events
         self.clients = clients
-        self.request.connection.set_close_callback(self.on_closing)
 
-    def on_closing(self):
-        self.events.remove('*', self.on_event)
+    def _on_closing(self):
+        self.events.remove('*', self._on_event)
 
-    def on_event(self, evt):
+    def _on_event(self, evt):
         self.write("data: %s\n\n" % evt)
         self.flush()
 
     @asynchronous
     def get(self):
-
-        def closing():
-            print "Il va falloir rentrer, monsieur"
-
+        self.set_status(200)
         self.set_header("Content-Type", "text/event-stream")
         self.set_header("Cache-Control", "no-cache")
 
-        self.events.add('*', self.on_event)
+        self.events.add('*', self._on_event)
+        self.request.connection.set_close_callback(self._on_closing)
 
-    def on_finish(self):
-        print "Finishing", self
 
 if __name__ == "__main__":
     import sys
     events = Emitter()
     application = Application([
         (r"/", EventSourceHandler, dict(events=events, clients=[])),
-    ])
-    application.listen(8888)
+    ], debug=True)
+    server = HTTPServer(application, no_keep_alive=True, xheaders=True)
+    server.listen(8888)
     client = GraphiteClient(events=events)
     client.watch_for(sys.argv[1])
     IOLoop.instance().start()
