0ab6a2d99d2f3551d131607370e4fa48977af140
Mathieu Lecarme <mlecarme@bearstech.com>
2013-04-05 22:45:53 +0200

    Handling deconnection.

--- src/web.py
+++ src/web.py
@@ -5,20 +5,39 @@ from tornado.ioloop import IOLoop, PeriodicCallback
 # https://github.com/guyzmo/event-source-library/blob/master/eventsource/listener.py
 
 
+class Emitter(object):
+
+    def __init__(self):
+        self.chans = {}
+
+    def add(self, chan, cb):
+        if chan not in self.chans:
+            self.chans[chan] = []
+        self.chans[chan].append(cb)
+
+    def remove(self, chan, cb):
+        if chan in self.chans:
+            self.chans[chan].remove(cb)
+
+    def emit(self, chan, *args, **dargs):
+        if chan in self.chans:
+            for cb in self.chans[chan]:
+                cb(*args, **dargs)
+
+
 class GraphiteClient(object):
 
-    def __init__(self, tempo=10):
+    def __init__(self, events, tempo=10):
+        self.events = events
         self.targets = set()
         self.client = HTTPClient()
         self.loop = PeriodicCallback(self._fetch, tempo * 1000)
         self.loop.start()
 
     def _fetch(self):
-        print "toto"
         for url in self.targets:
             response = self.client.fetch(url)
-            print response.body
-
+            self.events.emit('*', len(response.body))
 
     def watch_for(self, url):
         self.targets.add(url)
@@ -26,22 +45,39 @@ class GraphiteClient(object):
 
 class EventSourceHandler(RequestHandler):
 
-    def initialize(self, clients):
+    def initialize(self, events, clients):
+        self.events = events
         self.clients = clients
+        self.request.connection.set_close_callback(self.on_closing)
 
-    @asynchronous
-    def get(self, action, target):
-        self.set_header("Content-Type", "text/event-stream")
-        self.set_header("Cache-Control", "no-cache")
+    def on_closing(self):
+        self.events.remove('*', self.on_event)
+
+    def on_event(self, evt):
+        self.write("data: %s\n\n" % evt)
         self.flush()
 
+    @asynchronous
+    def get(self):
+
+        def closing():
+            print "Il va falloir rentrer, monsieur"
+
+        self.set_header("Content-Type", "text/event-stream")
+        self.set_header("Cache-Control", "no-cache")
+
+        self.events.add('*', self.on_event)
+
     def on_finish(self):
-        pass
+        print "Finishing", self
 
 if __name__ == "__main__":
+    import sys
+    events = Emitter()
     application = Application([
-        (r"/", EventSourceHandler, dict(clients=[])),
+        (r"/", EventSourceHandler, dict(events=events, clients=[])),
     ])
     application.listen(8888)
-    client = GraphiteClient()
+    client = GraphiteClient(events=events)
+    client.watch_for(sys.argv[1])
     IOLoop.instance().start()
